71133277f1992977158c57a6cdedced0f403197f
Andreas Eirich <andreas.eirich@iu-study.org>
2026-06-26 21:59:00 +0000

    fix: 50MB upload limit, implement document delete, auto-delete files on removal

---
+++ backend/apps/students/signals.py
@@ -0,0 +1,11 @@
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+
+from apps.students.models import StudentDocument
+
+
+@receiver(post_delete, sender=StudentDocument)
+def delete_student_document_file(sender, instance, **kwargs):
+    """Physische Datei löschen, wenn das Dokument-Objekt entfernt wird."""
+    if instance.file:
+        instance.file.delete(save=False)
